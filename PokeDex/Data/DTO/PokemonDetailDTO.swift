e5a4a9517c9e12ea9bf9d6226cc105c32890264a
HwangSeokBeom <tjrqja0714@gmail.com>
2026-01-31 22:47:21 +0900

    [Feat]: DTO 구현

--- PokeDex/Data/DTO/PokemonDetailDTO.swift
+++ PokeDex/Data/DTO/PokemonDetailDTO.swift
@@ -5,3 +5,45 @@
 //  Created by Hwangseokbeom on 1/31/26.
 //
 
+import Foundation
+
+struct PokemonDetailDTO: Decodable {
+    let id: Int
+    let name: String
+    let height: Int
+    let weight: Int
+    let types: [TypeEntryDTO]
+}
+
+extension PokemonDetailDTO {
+    
+    struct TypeEntryDTO: Decodable {
+        let slot: Int
+        let type: TypeDTO
+    }
+    
+    struct TypeDTO: Decodable {
+        let name: String
+        let url: String
+    }
+}
+
+extension PokemonDetailDTO {
+
+    func toDomain(
+        koreanName: String
+    ) -> PokemonDetail {
+
+        let mappedTypes: [PokemonTypeName] = types.compactMap {
+            PokemonTypeName(rawValue: $0.type.name)
+        }
+
+        return PokemonDetail(
+            id: id,
+            koreanName: koreanName,
+            types: mappedTypes,
+            heightDM: height,
+            weightHG: weight
+        )
+    }
+}
--- PokeDex/Data/DTO/PokemonListDTO.swift
+++ PokeDex/Data/DTO/PokemonListDTO.swift
@@ -5,3 +5,16 @@
 //  Created by Hwangseokbeom on 1/31/26.
 //
 
+import Foundation
+
+struct PokemonListDTO: Decodable {
+    let count: Int
+    let next: String?
+    let previous: String?
+    let results: [PokemonListItemDTO]
+}
+
+struct PokemonListItemDTO: Decodable {
+    let name: String
+    let url: String
+}
